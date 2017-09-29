63c624d4b1b48d2520913cd8cd70bd7b114d04b7
Andy Parry <andy.parry@bnymellon.com>
2017-09-29 17:17:49 +0100

    Add some dialog and apiai

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # cautious-spoon-bot
 MSBot test
+
+
+https://docs.microsoft.com/en-us/bot-framework/nodejs/bot-builder-nodejs-dialog-waterfall
+
+
+https://github.com/Microsoft/BotBuilder-Samples/blob/master/Node/core-basicMultiDialog/bot.js
+
+https://chatbotslife.com/how-to-create-a-restaurant-chatbot-part-2-fabae87cf58f
+https://github.com/devkiran/RestaurantChatbot/blob/master/app.js
--- app.js
+++ app.js
@@ -1,22 +1,30 @@
 var restify = require('restify');
 var builder = require('botbuilder');
+var apiaiRecognizer = require('./apiai_recognizer');
+
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({appId: process.env.MICROSOFT_APP_ID, appPassword: process.env.MICROSOFT_APP_PASSWORD});
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function(session) {
+  session.beginDialog('greetings');
+});
+
+bot.dialog('greetings', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.endDialog(`Hello ${results.response}!`);
+    }
+]);
 
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url);
-});
-
-// Create chat connector for communicating with the Bot Framework Service
-var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+server.listen(process.env.port || process.env.PORT || 3978, function() {
+  console.log('%s listening to %s', server.name, server.url);
 });
 
 // Listen for messages from users
 server.post('/api/messages', connector.listen());
-
-// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
-});
