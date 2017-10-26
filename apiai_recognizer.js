7040486fd6122577bb5b83d84999fa2c50c818e7
Andy Parry <andy@rodeocat.co.uk>
2017-10-26 13:22:45 +0100

    latest changes

--- apiai_recognizer.js
+++ apiai_recognizer.js
@@ -1,17 +1,23 @@
 var apiai = require('apiai');
 
-var app = apiai('<api.ai token>');
+var app = apiai('15327aa0af6d4404843372d9b09aa2ed');
 
 module.exports = {
   recognize: function(context, callback) {
-    var request = app.textRequest(context.message.text, {sessionId: Math.random()});
+    var request = app.textRequest(context.message.text, {sessionId:context.message.address.conversation.id});
 
     request.on('response', function(response) {
       var result = response.result;
-
       callback(null, {
         intent: result.metadata.intentName,
-        score: result.score
+        score: result.score,
+        entities: Object.keys(result.parameters)
+                    .filter(key => !!result.parameters[key])
+                    .map(key => ({
+                        entity: result.parameters[key],
+                        type: key,
+                        score: 1
+                    }))
       });
     });
 
--- app.js
+++ app.js
@@ -1,38 +1,49 @@
 var restify = require('restify');
 var builder = require('botbuilder');
-var apiaiRecognizer = require('./apiai_recognizer');
+//var apiaiRecognizer = require('./apiai_recognizer');
 
+var apiairecognizer = require('botbuilder-apiai');
+var recognizer = new apiairecognizer('15327aa0af6d4404843372d9b09aa2ed');
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({appId: process.env.MICROSOFT_APP_ID, appPassword: process.env.MICROSOFT_APP_PASSWORD});
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, {
-    persistConversationData: true
-});
+var bot = new builder.UniversalBot(connector, {persistConversationData: true});
 
-var intents = new builder.IntentDialog({
-    recognizers: [
-        apiaiRecognizer
-    ],
-    intentThreshold: 0.2,
-    recognizeOrder: builder.RecognizeOrder.series
-});
+var intents = new builder.IntentDialog({recognizers: [recognizer], intentThreshold: 0.2, recognizeOrder: builder.RecognizeOrder.series});
 
 intents.matches('Payment Status', '/payment.status');
-
+intents.matches('Default Welcome Intent', '/default.welcome');
+intents.matches('Default Fallback Intent', '/default.fallback');
 
 bot.dialog('/', intents);
 
-
-
-bot.dialog('/payment.status', [
-    function(session, args, next) {
-      console.log(args);
-        session.endDialog('Thank you for enquiring about your payment status.');
+bot.dialog('/default.welcome', [function(session, args, next) {
+    console.log(args);
+    var fulfillment = builder.EntityRecognizer.findEntity(args.entities, 'fulfillment');
+    if (fulfillment) {
+      var speech = fulfillment.entity;
+      session.endDialog(speech);
     }
+  }
 ]);
-
+bot.dialog('/default.fallback', [function(session, args, next) {
+    console.log(args);
+    var fulfillment = builder.EntityRecognizer.findEntity(args.entities, 'fulfillment');
+    if (fulfillment) {
+      var speech = fulfillment.entity;
+      session.endDialog(speech);
+    }
+  }
+]);
+bot.dialog('/payment.status', [function(session, args, next) {
+    console.log(args);
+    var vendor = builder.EntityRecognizer.findEntity(args.entities, 'vendor');
+    console.log(vendor);
+    session.endDialog('Thank you for enquiring about your payment status.');
+  }
+]);
 
 // Setup Restify Server
 var server = restify.createServer();
